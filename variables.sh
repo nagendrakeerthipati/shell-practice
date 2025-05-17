967bb768013a95899d260a739ae6e83c0d4ea075
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-17 21:34:00 +0530

    corrected errors

--- variables.sh
+++ variables.sh
@@ -3,5 +3,5 @@
 PERSON1=Nagendra
 PERSON2=sasi
 
-echo"$PERSON1 : how are you $PERSON2"
-echo"$PERSON2 : i am good what about $PERSON1"
+echo "$PERSON1 : how are you $PERSON2"
+echo "$PERSON2 : i am good what about $PERSON1"
