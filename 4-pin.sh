91ed57ff9a40b593b90f19fbed9cc40c8160b450
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-22 12:12:19 +0530

    practic

--- 4-pin.sh
+++ 4-pin.sh
@@ -2,6 +2,6 @@
 
 echo "please enter you pin number::"
 
-read PIN #THE PIN IS VARIABLE WE CAN GIVE IT FROM OUTSIDE CODE
+read -s PIN #THE PIN IS VARIABLE WE CAN GIVE IT FROM OUTSIDE CODE
 
 echo " your number is: $PIN"
