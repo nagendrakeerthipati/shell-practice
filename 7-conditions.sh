5475b6be9fb05fe3c02e7c19fd931237dc9df171
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-22 17:03:21 +0530

    practice

--- 7-conditions.sh
+++ 7-conditions.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [ "$(id -u)" -eq 0 ]; then
+if [ "$(id -u)" -eq 0 ] then
 
     echo "you are using root account"
+    exit 1 #give otherthan 0 upto 127
 else
     echo "you need root account to access"
 
 fi
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "installing mysql is ...success"
+else
+    echo "installing mysql is ...failure"
+    exit 1
+fi
