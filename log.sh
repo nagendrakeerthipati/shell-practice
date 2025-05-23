3a420ad6052fa6b0cef889cb66496c644f623040
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-23 19:33:16 +0530

    practice

--- log.sh
+++ log.sh
@@ -18,7 +18,7 @@ VALIDATE() {
     if [ $1 -eq 0 ]; then
         echo -e "$G Installing $2 is success $N"
     else
-        echo "$R installing $2 is ...failure $N"
+        echo -e "$R installing $2 is ...failure $N"
         exit 1
     fi
 }
@@ -27,27 +27,27 @@ VALIDATE() {
 dnf list installed mysql
 
 if [ $? -ne 0 ]; then
-    echo "$R mysql is not  installed ...going to install it $N" | tee -a $LOG_FILE
+    echo -e "$R mysql is not  installed ...going to install it $N" | tee -a $LOG_FILE
     dnf install mysql -y | tee -a $LOG_FILE
     VALIDATE $? "mysql"
 else
-    echo "$G mysql is already installed....nothing to do $N" | tee -a $LOG_FILE
+    echo -e "$G mysql is already installed....nothing to do $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed python3
 if [ $? -ne 0 ]; then
-    echo "$R python3 is not  installed ...going to install it $N" | tee -a $LOG_FILE
+    echo -e "$R python3 is not  installed ...going to install it $N" | tee -a $LOG_FILE
     dnf install python -y | tee -a $LOG_FILE
     VALIDATE $? "python3"
 else
-    echo "$Y python3 is already installed....nothing to do $N" | tee -a $LOG_FILE
+    echo -e "$Y python3 is already installed....nothing to do $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]; then
-    echo "$R nginx is not  installed ...going to install it $N" | tee -a $LOG_FILE
+    echo -e "$R nginx is not  installed ...going to install it $N" | tee -a $LOG_FILE
     dnf install nginx -y | tee -a $LOG_FILE
     VALIDATE $? "nginx"
 else
-    echo "$G nginx is already installed....nothing to do $N" | tee -a $LOG_FILE
+    echo -e "$G nginx is already installed....nothing to do $N" | tee -a $LOG_FILE
 fi
