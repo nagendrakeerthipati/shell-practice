5fe15ac948471fafaa331d8c7aa95acb6aa261c4
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-23 12:35:37 +0530

    practice

--- 8-functions.sh
+++ 8-functions.sh
@@ -11,7 +11,7 @@ fi
 
 VALIDATE() {
     if [ $1 -eq 0 ]; then
-        echo" Installing $2 is success"
+        echo "Installing $2 is success"
     else
         echo "installing $2 is ...failure"
         exit 1
@@ -24,7 +24,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]; then
     echo "mysql is not  installed ...going to install it"
     dnf install mysql -y
-    VALIDATE $1 "mysql"
+    VALIDATE $? "mysql"
 else
     echo "mysql is already installed....nothing to do"
 fi
@@ -33,7 +33,7 @@ dnf list installed python3
 if [ $? -ne 0 ]; then
     echo "python3 is not  installed ...going to install it"
     dnf install python -y
-    VALIDATE $1 "python3"
+    VALIDATE $? "python3"
 else
     echo "python3 is already installed....nothing to do"
 fi
@@ -42,7 +42,7 @@ dnf list installed nginx
 if [ $? -ne 0 ]; then
     echo "nginx is not  installed ...going to install it"
     dnf install nginx -y
-    VALIDATE $1 "nginx"
+    VALIDATE $? "nginx"
 else
     echo "nginx is already installed....nothing to do"
 fi
